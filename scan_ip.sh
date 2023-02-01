b1f5231b15df139b3447a335950e8c99cab32022
Jak13th <bertrand.t.kh@gmail.com>
2023-02-01 14:52:44 +0100

    feat: add no display output for ping

--- scan_ip.sh
+++ scan_ip.sh
@@ -7,7 +7,7 @@ base=${ipaddr:0:10}
 # Loop through the IP range and run ping to scan for open ports
 for i in {0..255..1}
 do
-	if ping -W 1 -c 1 $base$i
+	if ping -W 1 -c 1 $base$i 2>&1 >/dev/null
 	then echo $base$i >> online_ip.txt
 	else echo $base$i >> offline_ip.txt
 	fi

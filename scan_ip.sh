0f2b9dba375058b1a24d4092191f880ae58fc63b
Jak13th <bertrand.t.kh@gmail.com>
2023-02-01 14:41:30 +0100

    feat: scan_ip script added

---
+++ scan_ip.sh
@@ -0,0 +1,14 @@
+echo "Online IP adresses: " > online_ip.txt
+echo "Offline IP adresses: " > offline_ip.txt
+
+ipaddr=10.93.161.45
+base=${ipaddr:0:10}
+
+# Loop through the IP range and run ping to scan for open ports
+for i in {0..255..1}
+do
+	if ping -W 1 -c 1 $base$i
+	then echo $base$i >> online_ip.txt
+	else echo $base$i >> offline_ip.txt
+	fi
+done
